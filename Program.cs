fcb7d6ceef982b197bc266144386286296da1d21
Hanser Perez <hanselalfonzo@gmail.com>
2023-06-23 23:32:23 -0400

    Third Change

--- Program.cs
+++ Program.cs
@@ -429,6 +429,9 @@ LGB       `. | | | .'
         
         Console.WriteLine("Bienvenido al juego de adivinanzas de logotipos");
         Console.WriteLine("Cada respuesta correcta vale 4 puntos en el primer intento, de lo contrario ganarás 2 puntos.");
+
+        // Console.WriteLine("Ingrese el nombre Jugador 1");
+        // string jugador1 = Console.ReadLine();
         Console.WriteLine("Presione cualquier boton para continuar");
         Console.WriteLine();
         Console.ReadKey();
@@ -439,188 +442,283 @@ LGB       `. | | | .'
         //Crear aletoriedad
         
         Random rnd = new Random();
-        int seleccion, a=0,b=0,x=0;
+        int seleccion, x=0,a=0,b=0,d=0,e=0,c=0,f=0;
 
-        while (x < 3)
+        while (x < 7)
         {
 
-            seleccion = rnd.Next(1,5);
+            seleccion = rnd.Next(1,7);
 
-            while (seleccion == a || seleccion == b)
+            while (seleccion == a || seleccion == b || seleccion == c || seleccion == d || seleccion == e || seleccion == f)
             {
-            seleccion = rnd.Next(1,5);
+            seleccion = rnd.Next(1,7);
             }        
         
         if(x == 0)
         {
             a = seleccion;
         }
-
-        if(x == 1)
+        else if(x == 1)
         {
             b = seleccion;
         }
+        else if(x == 2)
+        {
+            c = seleccion;
+        }
+        else if(x == 3)
+        {
+            d = seleccion;
+        }
+        else if(x == 4)
+        {
+            e = seleccion;
+        }
+        else if(x == 5)
+        {
+            f = seleccion;
+        }
+
+        // if(x == 1)
+        // {
+        //     b = seleccion;
+        // }
         
         x += 1;
 
 
-        //---------------------------------------------------------------------------------------------------------------
-        //Inicio del juego
+            //---------------------------------------------------------------------------------------------------------------
+            //Inicio del juego
 
-        switch(seleccion)
-        {
-
-        case 1:
-
-        Console.WriteLine("Adivina el logotipo:");
-        Console.WriteLine(playBoy1);
-        string respuesta = Console.ReadLine();
-
-        if (respuesta == "Play boy")
-        {
-            puntos += 4;
-            Console.WriteLine("¡Respuesta correcta! Has ganado 4 puntos.");
-            Console.WriteLine(playBoy3);
-        }
-        else
-        {
-            Console.WriteLine("Respuesta incorrecta. Inténtalo nuevamente.");
-            Console.WriteLine(playBoy2);
-            respuesta = Console.ReadLine();
-
-            if (respuesta == "Play boy")
+            switch (seleccion)
             {
-                puntos += 2;
-                Console.WriteLine("¡Respuesta correcta! Has ganado 2 puntos.");
-                Console.WriteLine(playBoy3);
+
+                case 1:
+
+                    Console.WriteLine("Adivina el logotipo:");
+                    Console.WriteLine(playBoy1);
+                    string respuesta = Console.ReadLine()!;
+
+                    if (respuesta == "Play boy")
+                    {
+                        puntos += 4;
+                        Console.WriteLine("¡Respuesta correcta! Has ganado 4 puntos.");
+                        Console.WriteLine(playBoy3);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Respuesta incorrecta. Inténtalo nuevamente.");
+                        Console.WriteLine(playBoy2);
+                        respuesta = Console.ReadLine()!;
+
+                        if (respuesta == "Play boy")
+                        {
+                            puntos += 2;
+                            Console.WriteLine("¡Respuesta correcta! Has ganado 2 puntos.");
+                            Console.WriteLine(playBoy3);
+                        }
+                        else
+                        {
+
+                            Console.WriteLine("Respuesta incorrecta.");
+                            Console.WriteLine(playBoy3);
+                            Console.WriteLine("La respuesta correcta es 'Play Boy'");
+                        }
+                    }
+
+                    Console.ReadKey();
+                    break;
+
+
+                case 2:
+
+                    Console.WriteLine();
+                    Console.WriteLine("Adivina el segundo logotipo:");
+                    Console.WriteLine(jamesBond);
+                    respuesta = Console.ReadLine()!;
+
+                    if (respuesta == "James Bond")
+                    {
+                        puntos += 4;
+                        Console.WriteLine("¡Respuesta correcta! Has ganado 4 puntos.");
+                        Console.WriteLine(jamesBond3);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Respuesta incorrecta. Inténtalo nuevamente.");
+                        Console.WriteLine(jamesBond2);
+                        respuesta = Console.ReadLine()!;
+
+                        if (respuesta == "James Bond")
+                        {
+                            puntos += 2;
+                            Console.WriteLine("¡Respuesta correcta! Has ganado 2 puntos.");
+                            Console.WriteLine(jamesBond3);
+                        }
+                        else
+                        {
+
+                            Console.WriteLine("Respuesta incorrecta.");
+                            Console.WriteLine(playBoy3);
+                            Console.WriteLine("La respuesta correcta es 'James Bond'");
+                        }
+                    }
+
+                    Console.ReadKey();
+                    break;
+
+
+                case 3:
+
+                    Console.WriteLine();
+                    Console.WriteLine("Adivina el personaje de caricatura:");
+                    Console.WriteLine(calamardo1);
+                    respuesta = Console.ReadLine()!;
+
+                    if (respuesta == "Calamardo")
+                    {
+                        puntos += 4;
+                        Console.WriteLine("¡Respuesta correcta! Has ganado 4 puntos.");
+                        Console.WriteLine(calamardo3);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Respuesta incorrecta. Inténtalo nuevamente.");
+                        Console.WriteLine(calamardo2);
+                        respuesta = Console.ReadLine()!;
+
+                        if (respuesta == "Calamardo")
+                        {
+                            puntos += 2;
+                            Console.WriteLine("¡Respuesta correcta! Has ganado 2 puntos.");
+                            Console.WriteLine(calamardo3);
+                        }
+                        else
+                        {
+
+                            Console.WriteLine("Respuesta incorrecta.");
+                            Console.WriteLine(calamardo3);
+                            Console.WriteLine("La respuesta correcta es 'Calamardo'");
+                        }
+                    }
+
+                    Console.ReadKey();
+                    break;
+
+
+                case 4:
+
+                    Console.WriteLine();
+                    Console.WriteLine("Adivina el animal:");
+                    Console.WriteLine(oso1);
+                    respuesta = Console.ReadLine()!;
+
+                    if (respuesta == "Oso")
+                    {
+                        puntos += 4;
+                        Console.WriteLine("¡Respuesta correcta! Has ganado 4 puntos.");
+                        Console.WriteLine(oso3);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Respuesta incorrecta. Inténtalo nuevamente.");
+                        Console.WriteLine(oso2);
+                        respuesta = Console.ReadLine()!;
+
+                        if (respuesta == "Oso")
+                        {
+                            puntos += 2;
+                            Console.WriteLine("¡Respuesta correcta! Has ganado 2 puntos.");
+                            Console.WriteLine(oso3);
+                        }
+                        else
+                        {
+
+                            Console.WriteLine("Respuesta incorrecta.");
+                            Console.WriteLine(oso3);
+                            Console.WriteLine("La respuesta correcta es 'Oso'");
+                        }
+
+                    }
+                    Console.ReadKey();
+                    break;
+
+                case 5:
+
+                    Console.WriteLine();
+                    Console.WriteLine("Adivina la personaje:");
+                    Console.WriteLine();
+                    respuesta = Console.ReadLine()!;
+
+                    if (respuesta == "Correcaminos")
+                    {
+                        puntos += 4;
+                        Console.WriteLine("¡Respuesta correcta! Has ganado 4 puntos.");
+                        Console.WriteLine(Correcamino1);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Respuesta incorrecta. Inténtalo nuevamente.");
+                        Console.WriteLine(Correcamino2);
+                        respuesta = Console.ReadLine()!;
+
+                        if (respuesta == "Correcaminos")
+                        {
+                            puntos += 2;
+                            Console.WriteLine("¡Respuesta correcta! Has ganado 2 puntos.");
+                            Console.WriteLine(Correcamino3);
+                        }
+                        else
+                        {
+
+                            Console.WriteLine("Respuesta incorrecta.");
+                            Console.WriteLine(Correcamino3);
+                            Console.WriteLine("La respuesta correcta es 'Oso'");
+                        }
+                    }
+                    Console.ReadKey();
+                    break;
+
+                case 6:
+
+                    Console.WriteLine();
+                    Console.WriteLine("Adivina el Personaje:");
+                    Console.WriteLine(Bobeponga1);
+                    respuesta = Console.ReadLine()!;
+
+                    if (respuesta == "Bob esponja")
+                    {
+                        puntos += 4;
+                        Console.WriteLine("¡Respuesta correcta! Has ganado 4 puntos.");
+                        Console.WriteLine(Bobeponga1);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Respuesta incorrecta. Inténtalo nuevamente.");
+                        Console.WriteLine(Bobeponga2);
+                        respuesta = Console.ReadLine()!;
+
+                        if (respuesta == "Bob esponja")
+                        {
+                            puntos += 2;
+                            Console.WriteLine("¡Respuesta correcta! Has ganado 2 puntos.");
+                            Console.WriteLine(Bobeponga3);
+                        }
+                        else
+                        {
+
+                            Console.WriteLine("Respuesta incorrecta.");
+                            Console.WriteLine(Bobeponga3);
+                            Console.WriteLine("La respuesta correcta es 'Bob esponja");
+                        }
+                    }
+
+                    Console.ReadKey();
+                    break;
+
+
+
             }
-            else
-            {
-                
-                Console.WriteLine("Respuesta incorrecta.");
-                Console.WriteLine(playBoy3);
-                Console.WriteLine("La respuesta correcta es 'Play Boy'");
-            }
-        }
-
-        Console.ReadKey();
-        break;
-
-
-        case 2:
-
-        Console.WriteLine();
-        Console.WriteLine("Adivina el segundo logotipo:");
-        Console.WriteLine(jamesBond);
-        respuesta = Console.ReadLine();
-
-        if (respuesta == "James Bond")
-        {
-            puntos += 4;
-            Console.WriteLine("¡Respuesta correcta! Has ganado 4 puntos.");
-            Console.WriteLine(jamesBond3);
-        }
-        else
-        {
-            Console.WriteLine("Respuesta incorrecta. Inténtalo nuevamente.");
-            Console.WriteLine(jamesBond2);
-            respuesta = Console.ReadLine();
-
-            if (respuesta == "James Bond")
-            {
-                puntos += 2;
-                Console.WriteLine("¡Respuesta correcta! Has ganado 2 puntos.");
-                Console.WriteLine(jamesBond3);
-            }
-             else
-            {
-                
-                Console.WriteLine("Respuesta incorrecta.");
-                Console.WriteLine(playBoy3);
-                Console.WriteLine("La respuesta correcta es 'James Bond'");
-            }
-        }
-
-        Console.ReadKey();
-        break;
-
-
-        case 3:
-
-        Console.WriteLine();
-        Console.WriteLine("Adivina el personaje de caricatura:");
-        Console.WriteLine(calamardo1);
-        respuesta = Console.ReadLine();
-
-        if (respuesta == "Calamardo")
-        {
-            puntos += 4;
-            Console.WriteLine("¡Respuesta correcta! Has ganado 4 puntos.");
-            Console.WriteLine(calamardo3);
-        }
-        else
-        {
-            Console.WriteLine("Respuesta incorrecta. Inténtalo nuevamente.");
-            Console.WriteLine(calamardo2);
-            respuesta = Console.ReadLine();
-
-            if (respuesta == "Calamardo")
-            {
-                puntos += 2;
-                Console.WriteLine("¡Respuesta correcta! Has ganado 2 puntos.");
-                Console.WriteLine(calamardo3);
-            }
-             else
-            {
-                
-                Console.WriteLine("Respuesta incorrecta.");
-                Console.WriteLine(calamardo3);
-                Console.WriteLine("La respuesta correcta es 'Calamardo'");
-            }
-        }
-
-        Console.ReadKey();
-        break;
-
-
-        case 4:
-
-        Console.WriteLine();
-        Console.WriteLine("Adivina el animal:");
-        Console.WriteLine(oso1);
-        respuesta = Console.ReadLine();
-
-        if (respuesta == "Oso")
-        {
-            puntos += 4;
-            Console.WriteLine("¡Respuesta correcta! Has ganado 4 puntos.");
-            Console.WriteLine(oso3);
-        }
-        else
-        {
-            Console.WriteLine("Respuesta incorrecta. Inténtalo nuevamente.");
-            Console.WriteLine(oso2);
-            respuesta = Console.ReadLine();
-
-            if (respuesta == "Oso")
-            {
-                puntos += 2;
-                Console.WriteLine("¡Respuesta correcta! Has ganado 2 puntos.");
-                Console.WriteLine(oso3);
-            }
-             else
-            {
-                
-                Console.WriteLine("Respuesta incorrecta.");
-                Console.WriteLine(oso3);
-                Console.WriteLine("La respuesta correcta es 'Oso'");
-            }
-        }
-
-        Console.ReadKey();
-        break;
-
-        }
 
         }
 
@@ -631,6 +729,3 @@ LGB       `. | | | .'
 
     }
 }
-
-
-
